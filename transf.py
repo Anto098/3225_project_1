243b75c0bb72327a8d0da0ff701f1c3f49fb9f99
Anto <antonin.roy1@gmail.com>
2021-02-18 23:26:08 -0500

    fixed last page bug

--- transf.py
+++ transf.py
@@ -13,29 +13,33 @@ for link in soup.find_all("link"):
     if link.get("rel")[0] == "stylesheet":
         link["href"] = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  #Replacing old css stylesheet with bootstrap
 
-left_arrow = 0 #false
+left_arrow = False
+right_arrow = False
 for th in soup.find_all("th", {"class" : "c"}):
     all_as = th.find_all('a')
-    if(len(all_as)>1):
-        left_arrow = 1 # true
-        all_hrefs = []
-        for a in all_as: #get all hrefs
-            all_hrefs.append(a["href"])
+    all_hrefs = []
+    for a in all_as: #get all hrefs
+        if "<==" in a.text:
+            left_arrow = True
+        elif "==>" in a.text:
+            right_arrow = True
+        all_hrefs.append(a["href"])
     th.string = "" #remove everything in the th
-    if left_arrow == 1: #if there's a link on the left arrow, put a button in its stead
+    if left_arrow: #if there's a link on the left arrow, put a button in its stead
         left_a = soup.new_tag("a")
         left_a.string="PREVIOUS"
         left_a["class"] = ["btn btn-primary"]
         left_a["role"]=["button"]
         left_a["href"]=all_hrefs[0]
         th.append(left_a)
-    right_a = soup.new_tag("a")
-    right_a.string="NEXT"
-    right_a["class"] = ["btn btn-primary"]
-    right_a["role"]=["button"]
-    right_a["href"]=all_hrefs[left_arrow]  #lol at that index
-    th.append(right_a)
-        
+    if right_arrow:
+        right_a = soup.new_tag("a") #
+        right_a.string="NEXT"
+        right_a["class"] = ["btn btn-primary"]
+        right_a["role"]=["button"]
+        right_a["href"]=all_hrefs[left_arrow]  #lol at that index
+        th.append(right_a)
+            
 
 
 
@@ -46,11 +50,12 @@ for table in soup.find_all("table"): # change table style
     table["class"].append("table table-dark")
 
 for div in soup.find_all("div"): #make 3 blocs out of : html, css, html+css
-    if div["class"][0] == "testSource":
-        div["class"].append("d-flex mx-2  row")
-        for child in div.children:
-            if child != "\n":
-                child["class"].append("col border border-dark mx-3")
+    if div.has_attr("class"):
+        if div["class"][0] == "testSource":
+            div["class"].append("d-flex mx-2  row")
+            for child in div.children:
+                if child != "\n":
+                    child["class"].append("col border border-dark mx-3")
 
 
 
