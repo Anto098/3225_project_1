a14604b4e3d1adbb08f562d578d554e9d3aad34e
Anto <antonin.roy1@gmail.com>
2021-02-21 21:34:02 -0500

    removed ./ from run.sh

--- run.sh
+++ run.sh
@@ -9,10 +9,6 @@ else
 fi
 for full_path in "$1"/*
 do
-  echo "$full_path"
   file_name=$(echo "$full_path" | cut -d "/" -f2)
-  echo "$file_name"
-  python3 ./transf.py $full_path ./$2/$file_name
+  python3 transf.py ./$full_path ./$2/$file_name
 done
-
-
