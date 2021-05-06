d04232cf7ef2de3d579fc174ec52b13bacefc6b3
Anto <46535815+Anto098@users.noreply.github.com>
2021-05-06 15:10:37 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #$1 = dir with all files
 #$2 = output dir
-mkdir $2
-for file in $1:
+if [ -d $2 ]
+then
+	echo "dir already exists"
+else
+	mkdir $2
+fi
+for full_path in "$1"/*
 do
-	echo $file
-	python3 ./transf.py $file > ./$2/$file
+  file_name=$(echo "$full_path" | cut -d "/" -f2)
+  python3 ./transf.py $full_path ./$2/$file_name
 done
+
 
